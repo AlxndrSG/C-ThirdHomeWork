1a2498ce7555eccfade5bccbbb180fac971b87c2
Alexander <firefighter-911@rambler.ru>
2023-04-21 12:37:20 +0300

    Решение 21 задачи

---
+++ task21/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек 
+//и нахотид расстояние между ними в 3D пространстве.
+
+Console.Clear();
+
+int a1 = GetUserInput("Введите значение x1: ");
+int b1 = GetUserInput("Введите значение y1: ");
+int c1 = GetUserInput("Введите значение z1: ");
+int a2 = GetUserInput("Введите значение x2: ");
+int b2 = GetUserInput("Введите значение y2: ");
+int c2 = GetUserInput("Введите значение z3: ");
+
+double result = Math.Sqrt(Math.Pow(a2 - a1, 2) + Math.Pow(b2 - b1, 2) + Math.Pow(c2 - c1, 2));
+Console.WriteLine($"A ({a1},{b1},{c1}); B ({a2},{b2},{c2}) -> {result:f2}");
+
+static int GetUserInput(string message)
+{
+    while (true)
+    {
+        Console.Write(message);
+
+        if (int.TryParse(Console.ReadLine(), out int userInput))
+            return userInput;
+
+        Console.WriteLine("Вы ввели некорректные данные");
+    }
+}
