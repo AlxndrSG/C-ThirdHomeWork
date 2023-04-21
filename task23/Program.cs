05ecf8e343696b09eddcee7c306447d207dc2447
Alexander <firefighter-911@rambler.ru>
2023-04-21 12:37:52 +0300

    Решение 23 задачи

---
+++ task23/Program.cs
@@ -0,0 +1,30 @@
+﻿//Напишите программ, которая принимает на вход число (N) и
+//выдает таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+
+int number = GetNumberFromUser("Введите число: ", "Ошибка ввода!");
+PrintCube(number);
+
+void PrintCube(int cubeNumber)
+{
+    int index = 1;
+    while (index <= number)
+    {
+        double result = Math.Pow(index, 3);
+        Console.Write($"{result}, ");
+        index++;
+    }
+}
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+
+        if (int.TryParse(Console.ReadLine(), out int userNumber))
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
